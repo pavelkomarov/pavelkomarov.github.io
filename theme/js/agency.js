e62bc67e76d8451ad2dd406c1bb6a00fd04e2611
Pavel Komarov <pvlkmrv@gmail.com>
2018-12-20 15:57:40 -0500

    Update agency.js

--- theme/js/agency.js
+++ theme/js/agency.js
@@ -1,7 +1,7 @@
 
 // Set the action of the navbar buttons. jQuery for page page-scrolling feature
 // (requires jQuery Easing plugin); snap to correct url if not on home page.
-$('a.page-scroll').bind('click', function(event) {
+$('a.page-scroll').click(event => {
 	event.preventDefault();
 	let link = $(this).attr('href');
 	let site_root = window.location.protocol + '//' + window.location.host + '/';
@@ -22,7 +22,7 @@ $('body').scrollspy({ target: '.navbar', offset: 200 })
 
 // A function to fade-in the navbar's background on the homepage
 // based on vertical location
-var navbar_opacity = function() {
+function navbar_opacity() {
 	let offset = $(document).scrollTop();
 	let opacity = 1; // by default the background appears
 	if (window.location.href.match('^https?://.*/(#.*)?$')) {
@@ -34,11 +34,10 @@ var navbar_opacity = function() {
 	}
 	$('.navbar').css('background-color', 'rgba(34,34,34,'+opacity+')');
 }
-$(window).bind('scroll', navbar_opacity);
-$(window).bind('load', navbar_opacity);
-
+$(window).scroll(navbar_opacity);
+$(window).on('load', navbar_opacity);
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul a').click(function() {
+$('.navbar-collapse ul a').click(() => {
 	$('.navbar-toggler:visible').click();
 });
